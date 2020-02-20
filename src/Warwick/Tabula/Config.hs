b332f97992728ae946f70d4e05534347ab5984b3
Michael Gale <m.gale@warwick.ac.uk>
2020-02-20 19:58:33 +0000

    Add Tabula dev instance to known instances

--- src/Warwick/Tabula/Config.hs
+++ src/Warwick/Tabula/Config.hs
@@ -6,7 +6,8 @@
 module Warwick.Tabula.Config (
     TabulaInstance(..),
     liveURL,
-    sandboxURL
+    sandboxURL,
+    devURL
 ) where
 
 --------------------------------------------------------------------------------
@@ -24,17 +25,19 @@ import Warwick.Common
 -------------------------------------------------------------------------------
 
 -- | Enumerates Tabula instances.
-data TabulaInstance = Sandbox | Live | CustomInstance BaseUrl
+data TabulaInstance = Sandbox | Live | Dev | CustomInstance BaseUrl
 
 instance ToJSON TabulaInstance where 
     toJSON Sandbox = String "sandbox"
     toJSON Live    = String "live"
+    toJSON Dev     = String "dev"
     toJSON (CustomInstance url) = 
         object [ "custom" .= url ]
 
 instance FromJSON TabulaInstance where 
     parseJSON (String "sandbox") = pure Sandbox 
-    parseJSON (String "live")    = pure Live 
+    parseJSON (String "live")    = pure Live
+    parseJSON (String "dev")     = pure Dev  
     parseJSON val = flip (withObject "TabulaInstance") val $ \obj -> 
         CustomInstance <$> obj .: "custom"
 
@@ -46,9 +49,14 @@ liveURL = BaseUrl Https "tabula.warwick.ac.uk" 443 "/api/v1"
 sandboxURL :: BaseUrl
 sandboxURL = BaseUrl Https "tabula-sandbox.warwick.ac.uk" 443 "/api/v1"
 
+-- | The URL to the Tabula-dev API.
+devURL :: BaseUrl
+devURL = BaseUrl Https "tabula-dev.warwick.ac.uk" 443 "/api/v1"
+
 instance HasBaseUrl TabulaInstance where
     getBaseUrl Sandbox              = sandboxURL
     getBaseUrl Live                 = liveURL
+    getBaseUrl Dev                  = devURL
     getBaseUrl (CustomInstance url) = url
 
 --------------------------------------------------------------------------------
