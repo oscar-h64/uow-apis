9b8d035752ddc234b16a6327d05513b623724251
Oscar Harris <ozj.harris@gmail.com>
2020-06-02 20:58:57 +0100

    Remove `boolToLowerText`

--- src/Warwick/Sitebuilder/PageUpdate.hs
+++ src/Warwick/Sitebuilder/PageUpdate.hs
@@ -9,7 +9,7 @@ module Warwick.Sitebuilder.PageUpdate where
 
 import Data.Maybe (catMaybes)
 import Data.List (intersperse)
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import Data.XML.Types 
 
 import Text.Atom.Feed
@@ -39,26 +39,21 @@ data PageOptions = PageOptions {
     poEditComment :: Maybe Text
 } deriving Show
 
--- | 'boolToLowerText' @bool@ converts @bool@ to lowercase text
-boolToLowerText :: Bool -> Text
-boolToLowerText True  = "true"
-boolToLowerText False = "false"
-
 -- | 'optsToXML' @opts@ converts @opts@ to an array of XML elements
 optsToXML :: PageOptions -> [Data.XML.Types.Element]
 optsToXML PageOptions{..} = catMaybes [
-        xmlTextContent "sitebuilder:searchable" <$> (TextString . boolToLowerText <$> poSearchable),
-        xmlTextContent "sitebuilder:visibility" <$> (TextString . boolToLowerText <$> poVisible),
-        xmlTextContent "sitebuilder:span-rhs" <$> (TextString . boolToLowerText <$> poSpanRHS),
-        xmlTextContent "sitebuilder:deleted" <$> (TextString . boolToLowerText <$> poDeleted),
+        xmlTextContent "sitebuilder:searchable" <$> (TextString . pack . show <$> poSearchable),
+        xmlTextContent "sitebuilder:visibility" <$> (TextString . pack . show <$> poVisible),
+        xmlTextContent "sitebuilder:span-rhs" <$> (TextString . pack . show <$> poSpanRHS),
+        xmlTextContent "sitebuilder:deleted" <$> (TextString . pack . show <$> poDeleted),
         xmlTextContent "sitebuilder:description" <$> (TextString <$> poDescription),
         xmlTextContent "sitebuilder:keywords" <$> (TextString . mconcat . intersperse ", " <$> poKeywords),
         xmlTextContent "sitebuilder:link-caption" <$> (TextString <$> poLinkCaption),
         xmlTextContent "sitebuilder:page-heading" <$> (TextString <$> poPageHeading),
         xmlTextContent "sitebuilder:title-bar-caption" <$> (TextString <$> poTitleBarCaption),
         xmlTextContent "sitebuilder:page-order" <$> (TextString <$> poPageOrder),
-        xmlTextContent "sitebuilder:commentable" <$> (TextString . boolToLowerText <$> poCommentable),
-        xmlTextContent "sitebuilder:comments-visible-to-commenters-only" <$> (TextString . boolToLowerText <$> poCommentsVisibleToCommentersOnly),
+        xmlTextContent "sitebuilder:commentable" <$> (TextString . pack . show <$> poCommentable),
+        xmlTextContent "sitebuilder:comments-visible-to-commenters-only" <$> (TextString . pack . show <$> poCommentsVisibleToCommentersOnly),
         xmlTextContent "sitebuilder:layout" <$> (TextString <$> poLayout),
         xmlTextContent "sitebuilder:edit-comment" <$> (TextString <$> poEditComment)
     ]
