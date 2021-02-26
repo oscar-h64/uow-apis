336e831c93882c97101c0223552e52ce04c8c628
Michael Gale <m.gale@warwick.ac.uk>
2021-02-26 11:50:36 +0000

    Add Eq and Show instances for TabulaInstance

--- src/Warwick/Tabula/Config.hs
+++ src/Warwick/Tabula/Config.hs
@@ -1,7 +1,9 @@
---------------------------------------------------------------------------------
--- Haskell bindings for the University of Warwick APIs                        --
--- Copyright 2019 Michael B. Gale (m.gale@warwick.ac.uk)                      --
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-- Haskell bindings for the University of Warwick APIs                       --
+-------------------------------------------------------------------------------
+-- This source code is licensed under the MIT licence found in the           --
+-- LICENSE file in the root directory of this source tree.                   --
+-------------------------------------------------------------------------------
 
 module Warwick.Tabula.Config (
     TabulaInstance(..),
@@ -24,6 +26,7 @@ import Warwick.Common
 
 -- | Enumerates Tabula instances.
 data TabulaInstance = Sandbox | Live | Dev | CustomInstance BaseUrl
+    deriving (Eq, Show)
 
 instance ToJSON TabulaInstance where 
     toJSON Sandbox = String "sandbox"
